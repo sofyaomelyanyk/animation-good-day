ae328d3566c385aa4548e1b72721f33452ebf195
Sofya Omelyanyk <sofyaomelyanyk@gmail.com>
2023-03-13 12:33:28 +0200

    add changes

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ VANTA.BIRDS({
   color2: 0x4e8b9f,
 });
 
+const date = new Date();
+const hours = date.getHours();
+console.log("Date: ", hours);
+
 const openModal = document.querySelector("a[data-open]");
 const closeModal = document.querySelector("button[data-close]");
 const modal = document.querySelector("[data-modal]");
 const button = document.querySelector("[data-button]");
+const modalTitle = document.querySelector(".modal__title");
+
+console.log(modalTitle.textContent);
 
 console.log(openModal);
 
@@ -25,11 +32,21 @@ closeModal.addEventListener("click", onCloseModal);
 function onOpenModal() {
   modal.classList.remove("backdrop-hidden");
   button.classList.toggle("backdrop-hidden");
-  console.log("HIIIII");
+
+  if (hours >= 6 && hours <= 11) {
+    modalTitle.textContent = "Добрий ранок! Гарного тобі дня!";
+  } else if (hours >= 12 && hours <= 21) {
+    modalTitle.textContent = "Я радий тебе бачити! Заходь частіше!";
+  } else if (hours >= 22 && hours <= 1) {
+    modalTitle.textContent = "Здається вже запізно,  час лягати! Добраніч!";
+  } else {
+    modalTitle.textContent = "Ти чого тут ходиш? Бачив яка зараз година!?";
+  }
+ 
 }
 
 function onCloseModal() {
   modal.classList.add("backdrop-hidden");
   button.classList.toggle("backdrop-hidden");
-  console.log("BYEEE");
+  
 }
